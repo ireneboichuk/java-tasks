68b45b1797427011d1a31dc9a7f87e86b332a44f
Iryna Boichuk <irena8956@gmail.com>
2022-12-19 21:55:26 +0200

    Hometask 12

--- Lesson12/Main.java
+++ Lesson12/Main.java
@@ -15,14 +15,12 @@ public class Main {
         Scanner sc = new Scanner(in);
         out.println("Введіть значення рядка для перевірки:");
         String value = sc.nextLine();
-        Predicate<String> checkIsEmpty = str -> str.isEmpty();   // OR method reference can be used instead: String::isEmpty;
-        out.println("Введене значення є пустим: " + checkIsEmpty.test(value));
+        out.println(new Main().checkIsEmpty().test(value));
 
 
         // Task 2: Написати метод, який повертає {@link Supplier}, який завжди надає "Hello"
         out.println("Task 2:");
-        Supplier<String> hi = () -> "Hello!";
-        out.println(hi.get());
+        out.println(new Main().returnHello().get());
 
 
         /* Task 3: Написати метод, який повертає {@link Function}, яка приймає {@link String} і повертає цей рядок, що
@@ -31,26 +29,22 @@ public class Main {
         out.println("Введіть значення рядка:");
         String str = sc.nextLine();
         out.println("Введіть число для повторів рядка:");
-        int number = Integer.valueOf(sc.nextInt());
-        BiFunction<Integer, String, String> returnStringNTimes = (Integer num, String strr) -> {
-                return (strr.repeat(num) + "\n");
-        };
-        out.println(returnStringNTimes.apply(number, str));
+        int number = sc.nextInt();
+        out.println(new Main().returnStringNTimes(str).apply(number));
 
 
         // Task 4: Написати метод, який повертає {@link Supplier} випадкових цілих чисел
         out.println("Task 4:");
-        Supplier<Integer> intRandomNumbers = () -> new Random().nextInt(100);
-        out.println("Введіть кількість чисел для виведення:");
-        int numberOfNumbers = sc.nextInt();
-        for (int i = 0; i < numberOfNumbers; i++) {
-            out.println(intRandomNumbers.get());
-        }
+        out.println(new Main().randomIntNumbers(10).get());
 
 
-        /* Task 5: Написати метод, який отримує два параметри, які представляють діапазон, і повертає {@link Predicate<String>},
+        /* Task 5-1: Написати метод, який отримує два параметри, які представляють діапазон, і повертає {@link Predicate<String>},
         який перевіряє, чи довжина рядка відповідає вказаному діапазону. наприклад min <= length < max  */
         out.println("Task 5:");
+        out.println(new Main().CheckStringLength1(2, 10).test("Test Test"));
+
+
+        // Task 5-2 реалізація через новий функціональний інтерфейс:
         @FunctionalInterface
         interface CheckStringLength <T, U, V, R> {
             R apply (T t, U u, V v);
@@ -64,7 +58,31 @@ public class Main {
 
         // Task 6: Написати метод, який cтворює екземпляр {@link Optional<String>} за допомогою текстового параметра String
         out.println("Task 6:");
-        Optional<String> newText = Optional.of("test");
-        out.println(newText.get());
+        out.println(new Main().newText("test").get());
+    }
+
+
+    public static Predicate<String> checkIsEmpty () {
+        return String::isEmpty;
+    }
+
+    public Supplier<String> returnHello () {
+        return () -> "Hello!";
+    }
+
+    public Function<Integer, String> returnStringNTimes (String str) {
+        return str::repeat;
+    }
+
+    public Supplier<Integer> randomIntNumbers (int bound) {
+        return () -> new Random().nextInt(bound);
+    }
+
+    public Predicate<String> CheckStringLength1 (Integer min, Integer max) {
+       return (String str) -> ((min <= str.length()) && (str.length() < max));
+    }
+
+    public Optional<String> newText (String str) {
+        return Optional.of(str);
     }
 }
